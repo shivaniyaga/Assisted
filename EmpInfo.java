56cd30725f232161b2aa146bc0d1d5bce893d420
shivaniyaga <97832661+shivaniyaga@users.noreply.github.com>
2022-01-28 22:09:04 +0530

    Add files via upload

---
+++ EmpInfo.java
@@ -0,0 +1,24 @@
+package assisted;
+
+public class EmpInfo {
+	
+		int id;
+		String name;
+
+	void display() {
+		System.out.println(id+" "+name);
+		}
+	
+
+	
+
+	public static void main(String[] args) {
+
+		EmpInfo emp1=new EmpInfo();
+		EmpInfo emp2=new EmpInfo();
+
+		emp1.display();
+		emp2.display();
+		}
+
+}
